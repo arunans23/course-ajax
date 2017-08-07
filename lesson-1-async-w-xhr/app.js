45b2375235635802f2263a023ca76a58eb37b98f
Arunan <arunans23@gmail.com>
2017-08-07 13:16:20 +0530

    complete lesson 1

--- lesson-1-async-w-xhr/app.js
+++ lesson-1-async-w-xhr/app.js
@@ -8,5 +8,58 @@
         e.preventDefault();
         responseContainer.innerHTML = '';
         searchedForText = searchField.value;
+
+        const unsplashRequest = new XMLHttpRequest();
+
+        unsplashRequest.open('GET', `https://api.unsplash.com/search/photos?page=1&query=${searchedForText}`);
+        unsplashRequest.onload = addImage;
+        unsplashRequest.onerror = function(err){
+          console.log(err);
+        }
+        unsplashRequest.setRequestHeader('Authorization', 'Client-ID 3587b523b1766fefb76af778545514ed37ba58211895ca36c1725e2490738028');
+        unsplashRequest.send();
+
+
+
+
+        const articleRequest = new XMLHttpRequest();
+        articleRequest.onload = addArticles;
+        articleRequest.onerror = function(err){
+          console.log(err);
+        }
+        articleRequest.open('GET', `http://api.nytimes.com/svc/search/v2/articlesearch.json?q=${searchedForText}&api-key=5daba748a51c4855bedd6d5399b476d3`);
+        articleRequest.send();
+
+
     });
-})();
+
+    function addImage(){
+      let htmlContent = '';
+      const data = JSON.parse(this.responseText);
+      const firstImage = data.results[0];
+
+      htmlContent = `<figure>
+        <img src="${firstImage.urls.regular}" alt="${searchedForText}">
+        <figcaption>${searchedForText} by ${firstImage.user.name}</figcaption>
+      </figure>`;
+
+      responseContainer.insertAdjacentHTML('afterbegin', htmlContent);
+    }
+
+    function addArticles () {
+      let htmlContent = '';
+      const data = JSON.parse(this.responseText);
+
+      if (data.response && data.response.docs && data.response.docs.length > 1){
+        htmlContent = '<ul>' + data.response.docs.map(article => `<li class="article">
+          <h1><a href="${article.web_url}">${article.headline.main}</a></h2>
+          <p>${article.snippet}</p>
+        </li>`).join('') + '</ul>';
+      } else {
+        htmlContent = '<div class = "error-no-articles">No articles available</div>'
+      }
+
+      responseContainer.insertAdjacentHTML('beforeend', htmlContent);
+    }
+
+  })();
